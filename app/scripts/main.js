7ed08aeb540d7028f7d1e4b918f96818fdc75061
Lucas D'Avila <lucas@lucasdavi.la>
2013-12-12 00:06:26 -0200

    First commit.

---
+++ app/scripts/main.js
@@ -0,0 +1,18 @@
+/*global sphero, $*/
+
+
+window.sphero = {
+    Models: {},
+    Collections: {},
+    Views: {},
+    Routers: {},
+    init: function () {
+        'use strict';
+        console.log('Hello from Backbone!');
+    }
+};
+
+$(document).ready(function () {
+    'use strict';
+    sphero.init();
+});
