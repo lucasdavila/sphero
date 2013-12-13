3940142b5ad685d9ab54ad760018bf373a113252
Lucas D'Avila <lucas@lucasdavi.la>
2013-12-13 12:16:09 -0200

    Users now can vote on assets.

--- app/scripts/views/asset.js
+++ app/scripts/views/asset.js
@@ -7,11 +7,43 @@ sphero.Views = sphero.Views || {};
 
     sphero.Views.AssetView = Chute.View.extend({
 
+        initialize: function() {
+          this.listenTo(this, 'render', this.fixElPosition);
+        },
+
         template: function(data) {
           if (typeof this.parent.itemTemplate == 'function')
             return this.parent.itemTemplate(data);
 
           return JST['app/scripts/templates/asset.ejs'](data);
+        },
+
+        events: {
+          'mouseenter img': 'showCaption',
+          'mouseleave img': 'hideCaption',
+          'click a.vote': 'vote'
+        },
+
+        showCaption: function() {
+          this.$el.find('.caption').slideDown();
+        },
+
+        hideCaption: function() {
+          if (this.$el.find('.caption:hover').length > 0)
+            return;
+
+          this.$el.find('.caption').slideUp();
+        },
+
+        vote: function(e) {
+          e.preventDefault();
+          this.styleAttr = this.$el.attr('style');
+
+          this.model.heart();
+        },
+
+        fixElPosition: function() {
+          $('.item:not([style])').attr('style', this.styleAttr);
         }
 
     });
