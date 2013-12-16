1408f5e6ccc61f513904ca5bdd5f105246694d46
Lucas D'Avila <lucas@lucasdavi.la>
2013-12-16 12:04:45 -0200

    Added steps to upload.
    * Now user need to fill their profile, accept the privacy police select
      the video and see a thank you message.

--- app/scripts/views/app.js
+++ app/scripts/views/app.js
@@ -21,7 +21,52 @@ sphero.Views = sphero.Views || {};
 
           var chooser = new Chute.Chooser({
             client_id: '4f0b30a938ecef2020000003',
-            album: 'arOOyzse'
+            album: 'arOOyzse',
+            steps: ['profile', 'privacy', 'selector', 'thanks'],
+            stepOptions: {
+              profile: {
+                title : 'Profile',
+                next : 'Next',
+                fields: [{
+                  type: 'text',
+                  name: 'name',
+                  label: 'Name',
+                  required: true
+                }, {
+                  type: 'text',
+                  name: 'email',
+                  label: 'Email',
+                  required: true,
+                  match: '^([^@\\s]+)@((?:[-a-z0-9]+\\.)+[a-z]{2,})$'
+                }, {
+                  type: 'text',
+                  name: 'Date of Birth',
+                  label: 'Date of Birth (MM-DD-YYYY)',
+                  required: true,
+                  match: '^([0-9]){1,2}-([0-9]){1,2}-([0-9]){4}$'
+                }, {
+                  type: 'checkbox',
+                  name: 'newsletter',
+                  label: 'Yes! I\'d like to join the Sphero newsletter!',
+                  required: false
+                }]
+              },
+              privacy: {
+                title: 'Please read and agree to the privacy policy',
+                next: 'I Agree',
+                template: '<a href="http://www.gosphero.com/privacy/" target="_blank" style="color:black;text-decoration:underline;">Privacy Policy</a>'
+              },
+              selector: {
+                title : 'Select Your Videos',
+                next : 'Next',
+                services : ['upload', 'facebook', 'instagram']
+              },
+              thanks : {
+                title : 'Thank you',
+                next : 'Done',
+                text : 'Thank you! Your videos have been successfully uploaded.'
+              }
+            }
           });
 
           chooser.on('complete', function(data){
